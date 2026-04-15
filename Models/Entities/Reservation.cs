9b659ec2ea66edc1dc0434ae1a3302688fd432b1
adrianaOM3 <111209153+adrianaOM3@users.noreply.github.com>
2026-04-15 04:03:37 -0600

    Initial set reservation
    The DTOS for reservation requests and responses, reservation mappers, reservation entities, and reservation repositories were created.

--- Models/Entities/Reservation.cs
+++ Models/Entities/Reservation.cs
@@ -3,7 +3,7 @@ namespace reservations_api.Models.Entities;
 public class Reservation
 {
   public Guid Id { get; set; }
-  //public Guid ClassroomId { get; set; }
+  public Guid ClassroomId { get; set; }
   public DateOnly Date { get; set; }
   public TimeOnly StartTime { get; set; }
   public TimeOnly EndTime { get; set; }
